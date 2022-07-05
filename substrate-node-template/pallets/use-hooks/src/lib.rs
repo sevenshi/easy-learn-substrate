b340066b4779d42cbaab9129b1507abd0673a885
SevenShi <seven.shi88@gmail.com>
2022-07-05 10:16:00 +0800

    add use hooks

---
+++ substrate-node-template/pallets/use-hooks/src/lib.rs
@@ -0,0 +1,96 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/v3/runtime/frame>
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{pallet_prelude::*, transactional};
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/v3/runtime/storage
+	#[pallet::storage]
+	#[pallet::getter(fn my_param)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
+	pub type Param<T: Config> = StorageValue<_, u32, ValueQuery>;
+
+	#[pallet::storage]
+	pub type SetFalg<T: Config> = StorageValue<_, bool, ValueQuery>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		SetParam(u32),
+	}
+
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(n: BlockNumberFor<T>) -> Weight {
+			log::info!(target: "use-hooks", 
+			"++++++++++++ on_initialize, block number is {:?}", n);
+			0
+		}
+
+		fn on_finalize(n: BlockNumberFor<T>)  {
+			log::info!(target: "use-hooks", 
+			"++++++++++++ on_finalize, block number is {:?}", n);
+		}
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		ParamInvalid,
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		///
+		#[transactional]
+		#[pallet::weight(0)]
+		pub fn set_param_bigger_than_100(origin: OriginFor<T>, param: u32) -> DispatchResult {
+			ensure_signed(origin)?;
+
+			// Update storage.
+			SetFalg::<T>::put(true);
+
+			if param <= 100u32 {
+				return Err(Error::<T>::ParamInvalid.into());
+			}
+			Param::<T>::put(param);
+
+			// Emit an event.
+			Self::deposit_event(Event::SetParam(param));
+
+			log::info!(target: "use-hooks", 
+			"set param bigger then 100");
+
+			Ok(())
+		}
+	}
+}
